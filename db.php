6348d529252ce03cdfd720dee5dd7427ed40564a
Abbas <a.samadani256@gmail.com>
2021-03-24 21:47:34 +0000

    complete insert and select query in db class

--- db.php
+++ db.php
@@ -8,6 +8,9 @@ class db
     private $db;
     private $user;
     private $pass;
+    private $tbl;
+
+
     public function __construct($server='localhost', $db='phonebook', $user='root', $pass='')
     {
         $this->server=$server;
@@ -26,6 +29,38 @@ class db
         }
 
     }
+
+    public function setTbl($tbl)
+    {
+        $this->tbl = $tbl;
+    }
+
+    function select_data($name='*'){
+        if(is_array($name)){
+            $names = implode(",",$name);
+        }else{
+            $names=$name;
+        }
+        $sql = $this->pdh->prepare("SELECT {$names} FROM {$this->tbl}");
+        $sql->execute();
+        $row=$sql->fetchAll(PDO::FETCH_OBJ);
+        var_dump($row);
+    }
+
+    function insert_data($field,$data){
+        if(is_array($data)){
+            $names = "'".implode("','",$data)."'";
+            $field = implode(",",$field);
+            $sql = $this->pdh->prepare("INSERT INTO {$this->tbl}($field) VALUES ($names)");
+            //var_dump($sql);
+            $sql->execute();
+
+        }
+
+    }
 }
 
 $obj = new db();
+$obj->setTbl("user_tbl");
+$obj->select_data();
+//$obj->insert_data(['name','email','password','lastname'],['ebi','ebi@gmail.com','123','rezaie']);
